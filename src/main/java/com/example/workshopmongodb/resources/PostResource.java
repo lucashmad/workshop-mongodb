3f4d44cbf6ea03df92dceaf2984e1cddb4f336e3
Lucas Moreira <lucaslm.andrade1245@gmail.com>
2024-06-02 19:43:23 -0300

    usando consultas com query methods

--- src/main/java/com/example/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/example/workshopmongodb/resources/PostResource.java
@@ -1,13 +1,15 @@
 package com.example.workshopmongodb.resources;
 
 import com.example.workshopmongodb.domain.Post;
+import com.example.workshopmongodb.repository.util.URL;
 import com.example.workshopmongodb.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.List;
 
 
 @RestController
@@ -22,4 +24,12 @@ public class PostResource {
         Post post = postService.findById(id);
         return ResponseEntity.ok().body(post);
     }
+
+    @GetMapping(value = "titlesearch")
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
+        text = URL.decodeParam(text);
+        List<Post> postList = postService.findByTitle(text);
+        return ResponseEntity.ok().body(postList);
+    }
+
 }
